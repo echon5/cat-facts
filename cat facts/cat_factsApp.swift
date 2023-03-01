6d5a80682654916b0194b21d6ca56b42b347eec6
Erin Chon <echon@bsd220.org>
2023-03-01 09:38:13 -0600

    Initial Commit

---
+++ cat facts/cat_factsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cat_factsApp.swift
+//  cat facts
+//
+//  Created by Erin Chon on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct cat_factsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
